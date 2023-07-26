65a949ea4e707c0ce5c8990f9d15f9c9648514eb
Vladialexxx <vladialex@inbox.ru>
2023-07-26 15:54:46 +0500

    3 / 3 задания ( 1 / 2 доп )

--- script.py
+++ script.py
@@ -8,45 +8,26 @@ def load_data():
     with open("RU.txt", "r", encoding="utf-8") as file:
         for line in file:
             fields = line.strip().split("\t")
-            geonameid = int(fields[0])
-            name = fields[1]
-            asciiname = fields[2]
-            alternatenames = fields[3]
-            latitude = fields[4]
-            longitude = fields[5]
-            feature_class = fields[6]
-            feature_code = fields[7]
-            country_code = fields[8]
-            cc2 = fields[9]
-            admin1_code = fields[10]
-            admin2_code = fields[11]
-            admin3_code = fields[12]
-            admin4_code = fields[13]
-            population = fields[14]
-            elevation = fields[15]
-            dem = fields[16]
-            timezone = fields[17]
-            modification_date = fields[18]
-
-            data[geonameid] = {
-                "name": name,
-                "asciiname": asciiname,
-                "alternatenames": alternatenames,
-                "latitude": latitude,
-                "longitude": longitude,
-                "feature_class": feature_class,
-                "feature_code": feature_code,
-                "country_code": country_code,
-                "cc2": cc2,
-                "admin1_code": admin1_code,
-                "admin2_code": admin2_code,
-                "admin3_code": admin3_code,
-                "admin4_code": admin4_code,
-                "population": population,
-                "elevation": elevation,
-                "dem": dem,
-                "timezone": timezone,
-                "modification_date": modification_date,
+            print(line)
+            data[int(fields[0])] = {
+                "name": fields[1],
+                "asciiname": fields[2],
+                "alternatenames": fields[3],
+                "latitude": fields[4],
+                "longitude": fields[5],
+                "feature_class": fields[6],
+                "feature_code": fields[7],
+                "country_code": fields[8],
+                "cc2": fields[9],
+                "admin1_code": fields[10],
+                "admin2_code": fields[11],
+                "admin3_code": fields[12],
+                "admin4_code": fields[13],
+                "population": fields[14],
+                "elevation": fields[15],
+                "dem": fields[16],
+                "timezone": fields[17],
+                "modification_date": fields[18],
             }
 
     return data
@@ -65,68 +46,126 @@ def get_cities_by_page(page, items_per_page):
     return cities
 
 
-# Метод принимает названия двух городов (на русском языке) и получает информацию о найденных городах,
-# а также дополнительно: какой из них расположен севернее и на сколько часов различается их временная зона.
+def city_analyzer(part_name):
+    result = []
+    for geonameid, info in data.items():
+        alternatenames = info['alternatenames'].lower().split(',')
+        if part_name.lower() in info['name'].lower():
+            result.append(info['name'])
+            continue
+        for alt_name in alternatenames:
+            if part_name.lower() in alt_name.strip():
+                result.append(alt_name)
+                break
+    return result
+
+
+def get_city_by_name(name):
+    result = []
+    for geonameid, info in data.items():
+        alternatenames = info['alternatenames'].lower().split(',')
+        if name.lower() == info['name'].lower():
+            result.append(info)
+            continue
+        for alt_name in alternatenames:
+            if name.lower() == alt_name.strip():
+                result.append(info)
+                break
+
+    if len(result) > 1:
+        return max(result, key=lambda city: int(city["population"]))
+    elif result:
+        return result[0]
+    else:
+        return None
+
+
 def compare_cities(city1, city2):
-    # Ваш код обработки запроса по названиям городов и возврат информации о городах
-    pass
+    city_info1 = get_city_by_name(city1)
+    city_info2 = get_city_by_name(city2)
+
+    if city_info1 is None or city_info2 is None:
+        return None
+
+    northern_city = city_info1 if city_info1["latitude"] > city_info2["latitude"] else city_info2
+    same_timezone = city_info1["timezone"] == city_info2["timezone"]
+
+    return {
+        "city1": city_info1,
+        "city2": city_info2,
+        "northern_city": northern_city["name"],
+        "same_timezone": same_timezone
+    }
 
 
 class GeoServer(http.server.SimpleHTTPRequestHandler):
     def do_GET(self):
-        # Извлечь путь из URL запроса
         path = urllib.parse.urlparse(self.path).path
-
-        # Разбить путь на компоненты, отделенные '/'
         path_components = path.split('/')
 
-        # Проверить, если запрос начинается с '/city/'
         if path_components[1] == 'city':
-            # Получить geonameid из запроса (предполагается, что geonameid находится вторым элементом)
             geonameid = int(path_components[2])
-
-            # Вызвать метод get_city_by_geonameid(geonameid) и получить информацию о городе
             city_info = get_city_by_geonameid(geonameid)
 
             if city_info:
-                # Отправить успешный ответ с информацией о городе в формате JSON
                 self.send_response(200)
                 self.send_header('Content-type', 'application/json')
                 self.end_headers()
                 self.wfile.write(str(city_info).encode())
             else:
-                # Отправить ответ с ошибкой, если город с указанным geonameid не найден
                 self.send_response(404)
                 self.send_header('Content-type', 'text/plain')
                 self.end_headers()
                 self.wfile.write(b'City not found')
 
-        # Добавить обработку запроса для получения списка городов по страницам
         elif path_components[1] == 'cities':
-            # Получить номер страницы из запроса (предполагается, что номер страницы находится вторым элементом,
-            # а количество городов на странице - третьим элементом)
             page_number = int(path_components[2])
             items_per_page = int(path_components[3])
-
-            # Вызвать метод get_cities_by_page(page_number, items_per_page)
-            # и получить список городов для данной страницы
             cities = get_cities_by_page(page_number, items_per_page)
 
             if cities:
-                # Отправить успешный ответ с информацией о городах в формате JSON
                 self.send_response(200)
                 self.send_header('Content-type', 'application/json')
                 self.end_headers()
                 self.wfile.write(str(cities).encode())
             else:
-                # Отправить ответ с ошибкой, если страница не найдена или другая ошибка
+                self.send_response(404)
+                self.send_header('Content-type', 'text/plain')
+                self.end_headers()
+                self.wfile.write(b'Page not found')
+
+        elif path_components[1] == 'compare':
+            citi1 = urllib.parse.unquote(path_components[2])
+            citi2 = urllib.parse.unquote(path_components[3])
+            cities = compare_cities(citi1, citi2)
+
+            if cities:
+                self.send_response(200)
+                self.send_header('Content-type', 'application/json')
+                self.end_headers()
+                self.wfile.write(str(cities).encode())
+            else:
+                self.send_response(404)
+                self.send_header('Content-type', 'text/plain')
+                self.end_headers()
+                self.wfile.write(b'Page not found')
+
+        elif path_components[1] == 'analis':
+            part_citi = urllib.parse.unquote(path_components[2])
+            cities = city_analyzer(part_citi)
+
+            if cities:
+                self.send_response(200)
+                self.send_header('Content-type', 'application/json')
+                self.end_headers()
+                self.wfile.write(str(cities).encode())
+            else:
                 self.send_response(404)
                 self.send_header('Content-type', 'text/plain')
                 self.end_headers()
                 self.wfile.write(b'Page not found')
 
         else:
-            # Отправить ответ с ошибкой для всех остальных запросов
             self.send_response(404)
             self.send_header('Content-type', 'text/plain')
             self.end_headers()
@@ -135,9 +174,6 @@ class GeoServer(http.server.SimpleHTTPRequestHandler):
 
 if __name__ == "__main__":
     data = load_data()
-
-    #for key, value in data.items():
-        #print(f'{value}')
 
     server_address = ('', 8000)
     httpd = http.server.HTTPServer(server_address, GeoServer)
